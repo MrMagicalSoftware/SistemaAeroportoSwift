3dc7dc7cd84705573d60247fa0ff784decc866c5
mr <cds>
2022-06-13 11:38:49 +0200

    Initial Commit

---
+++ SistemaAeroportoSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SistemaAeroportoSwift
+//
+//  Created by mr on 13/06/22.
+//  Copyright © 2022 mr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
